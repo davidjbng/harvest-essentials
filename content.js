365dd9771a2dd6811ee26bf5df811cdcf4702112
David Jebing <david.jebing@lise.de>
2022-11-18 14:44:13 +0100

    highlight with js working

--- content.js
+++ content.js
@@ -1,33 +1,29 @@
 console.log("Content script loaded");
 
-/** @type {HTMLElement[]} */
-const timeEntries = Array.from(
-  document.querySelectorAll("#day-view-entries > tbody tr")
-);
+/** @type {() => HTMLElement[]} */
+const getTimeEntries = () =>
+  Array.from(document.querySelectorAll("#day-view-entries > tbody tr"));
 
-console.log("Entries count: ", timeEntries.length);
-console.log("Entries: ", timeEntries);
+// const noNotesMarker = document.createElement("div");
+// noNotesMarker.style.height = "1rem";
+// noNotesMarker.style.color = "red";
+// noNotesMarker.style.fontSize = "2rem";
+// noNotesMarker.textContent = "No Notes";
 
-const noNotesMarker = document.createElement("div");
-noNotesMarker.style.height = "1rem";
-noNotesMarker.style.color = "red";
-noNotesMarker.style.fontSize = "2rem";
-noNotesMarker.textContent = "No Notes";
-
-const observer = new MutationObserver((mutations) => {
-  for (const mutation of mutations) {
-    if (mutation.type === "childList") {
-      console.log("A child node has been added or removed.", mutation);
-    } else if (mutation.type === "attributes") {
-      console.log(
-        `The ${mutation.attributeName} attribute was modified.`,
-        mutation
-      );
-    } else {
-      console.log("The element was modified", mutation);
-    }
-  }
-});
+// const observer = new MutationObserver((mutations) => {
+//   for (const mutation of mutations) {
+//     if (mutation.type === "childList") {
+//       console.log("A child node has been added or removed.", mutation);
+//     } else if (mutation.type === "attributes") {
+//       console.log(
+//         `The ${mutation.attributeName} attribute was modified.`,
+//         mutation
+//       );
+//     } else {
+//       console.log("The element was modified", mutation);
+//     }
+//   }
+// });
 
 // @ts-ignore
 // observer.observe(document.getElementById("day-view-entries"), {
@@ -38,10 +34,16 @@ const observer = new MutationObserver((mutations) => {
 
 // setTimeout(() => {
 //   highlightMissingNotes();
-// }, 5000);
+// });
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    highlightMissingNotes();
+  }, 1000);
+});
 
 function highlightMissingNotes() {
-  const withoutNotes = getMissingNoteEntries(timeEntries);
+  const withoutNotes = getMissingNoteEntries(getTimeEntries());
   console.log("Entries without notes: ", withoutNotes);
   withoutNotes.forEach((e) => {
     e.style.outline = "4px solid red";
